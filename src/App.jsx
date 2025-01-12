c726cbcdf7f864e8e83d889e1ab6cfc85805d61a
913122104307_santhoshkumar <ksdsanthosh130@gmail.com>
2025-01-12 16:41:42 +0530

    starting

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./Components/NavBar"
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <navbar/>
+      Hello world!
+    </h1>
+  )
+}
